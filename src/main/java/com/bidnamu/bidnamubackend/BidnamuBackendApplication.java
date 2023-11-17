fcde2d80d5228e5dda01cf2efaa7d11e96453575
kang815595@gmail.com <Kang815595@gmail.com>
2023-11-17 10:20:52 +0900

    Initial Commit

---
+++ src/main/java/com/bidnamu/bidnamubackend/BidnamuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bidnamu.bidnamubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidnamuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidnamuBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bidnamu/bidnamubackend/BidnamuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bidnamu.bidnamubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidnamuBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
