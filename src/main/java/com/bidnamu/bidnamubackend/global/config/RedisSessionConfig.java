847cbb505cfc3fd1d64da2f1d6cebb95cf0e8a1f
kang815595@gmail.com <Kang815595@gmail.com>
2023-11-20 18:06:32 +0900

    feat: Redis Session 설정

--- src/main/java/com/bidnamu/bidnamubackend/global/config/RedisSessionConfig.java
+++ src/main/java/com/bidnamu/bidnamubackend/global/config/RedisSessionConfig.java
@@ -3,10 +3,12 @@ package com.bidnamu.bidnamubackend.global.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
@@ -19,9 +21,8 @@ public class RedisSessionConfig {
     @Value("${spring.redis.session.port}")
     private int port;
 
-    @Primary
-    @Bean
-    public RedisConnectionFactory redisSessionConnectionFactory() {
+    @Bean({"redisConnectionFactory", "redisSessionConnectionFactory"})
+    public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration redisStandaloneConfiguration
             = new RedisStandaloneConfiguration();
         redisStandaloneConfiguration.setHostName(hostName);
@@ -29,4 +30,12 @@ public class RedisSessionConfig {
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
+    }
 }
