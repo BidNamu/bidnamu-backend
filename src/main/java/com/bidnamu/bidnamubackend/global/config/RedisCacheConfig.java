40e1dbc3f32c89d087a8ccc9fceef103bfc6eb31
kang815595@gmail.com <Kang815595@gmail.com>
2023-11-20 18:12:22 +0900

    feat: Redis Cache 설정

--- src/main/java/com/bidnamu/bidnamubackend/global/config/RedisCacheConfig.java
+++ src/main/java/com/bidnamu/bidnamubackend/global/config/RedisCacheConfig.java
@@ -1,15 +1,22 @@
 package com.bidnamu.bidnamubackend.global.config;
 
+import java.time.Duration;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@EnableRedisHttpSession
+@EnableCaching
 public class RedisCacheConfig {
 
     @Value("${spring.redis.cache.host}")
@@ -18,12 +25,39 @@ public class RedisCacheConfig {
     @Value("${spring.redis.cache.port}")
     private int port;
 
-    @Bean
-    public RedisConnectionFactory redisCacheConnectionFactory() {
+    private static final String CONNECTION_FACTORY = "redisCacheConnectionFactory";
+
+    @Bean(name = CONNECTION_FACTORY)
+    public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration redisStandaloneConfiguration
             = new RedisStandaloneConfiguration();
         redisStandaloneConfiguration.setHostName(hostName);
         redisStandaloneConfiguration.setPort(port);
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    /**
+     * cache 만료 : 하루
+     */
+    @Bean
+    public RedisCacheManager redisCacheManager(
+        @Qualifier(CONNECTION_FACTORY) RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration
+            .defaultCacheConfig()
+            .serializeKeysWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(
+                    new StringRedisSerializer())
+            )
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(
+                    new GenericJackson2JsonRedisSerializer())
+            )
+            .entryTtl(Duration.ofDays(1L));
+
+        return RedisCacheManager
+            .RedisCacheManagerBuilder
+            .fromConnectionFactory(redisConnectionFactory)
+            .cacheDefaults(redisCacheConfiguration)
+            .build();
     }
 }
