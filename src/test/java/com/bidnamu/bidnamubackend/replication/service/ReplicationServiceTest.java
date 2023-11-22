de12cd0594c8233c5e75ad8b15ea6edf99f68bfe
kang815595@gmail.com <Kang815595@gmail.com>
2023-11-22 14:45:45 +0900

    feat: DB replication 테스트 구성

--- src/test/java/com/bidnamu/bidnamubackend/replication/service/ReplicationServiceTest.java
+++ src/test/java/com/bidnamu/bidnamubackend/replication/service/ReplicationServiceTest.java
@@ -6,9 +6,7 @@ import com.bidnamu.bidnamubackend.testClass.TestService;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
