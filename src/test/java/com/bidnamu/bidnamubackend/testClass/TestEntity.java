4305bb971b65c5690eb407ed0b7dd635f4e487ac
kang815595@gmail.com <Kang815595@gmail.com>
2023-11-22 14:45:31 +0900

    feat: DB replication 테스트 구성

--- src/test/java/com/bidnamu/bidnamubackend/testClass/TestEntity.java
+++ src/test/java/com/bidnamu/bidnamubackend/testClass/TestEntity.java
@@ -4,16 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 @Getter
 public class TestEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Setter
+    private String data;
 }
