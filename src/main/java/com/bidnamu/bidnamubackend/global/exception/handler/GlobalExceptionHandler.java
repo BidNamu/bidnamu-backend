32fd32cdee99a9410189fc9adb56574893ab9f22
kimmin1kk <rudals1888@naver.com>
2023-11-23 22:00:40 +0900

    feat: MethodArgumentNotValidException 예외 처리 Handler 추가

--- src/main/java/com/bidnamu/bidnamubackend/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/bidnamu/bidnamubackend/global/exception/handler/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import com.bidnamu.bidnamubackend.global.exception.response.ErrorResponse;
 import com.bidnamu.bidnamubackend.user.exception.DuplicatedEmailException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,7 +14,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(DuplicatedEmailException.class)
-    public ResponseEntity<Object> handleDuplicationEmailException(DuplicatedEmailException e) {
+    public ResponseEntity<ErrorResponse> handleDuplicationEmailException(DuplicatedEmailException e) {
         final ErrorResponse errorResponse = ErrorResponse.builder()
                 .code(CommonErrorCode.INVALID_PARAMETER)
                 .message(e.getMessage())
@@ -20,6 +22,13 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
     }
 
-
-
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        FieldError error = e.getBindingResult().getFieldErrors().get(0);
+        final ErrorResponse errorResponse = ErrorResponse.builder()
+                .code(CommonErrorCode.INVALID_PARAMETER)
+                .message(error.getDefaultMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
+    }
 }
