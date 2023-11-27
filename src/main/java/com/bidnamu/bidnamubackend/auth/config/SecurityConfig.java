b9f7ca89d2daa833ee57c79b17c5d06461413bff
kimmin1kk <rudals1888@naver.com>
2023-11-27 17:36:44 +0900

    chore: SecurityConfig 변경 - 예외 핸들러 추가 - 세션 쿠키를 통한 인증 방식 사용 하지 않음에 따른 무상태 설정

--- src/main/java/com/bidnamu/bidnamubackend/auth/config/SecurityConfig.java
+++ src/main/java/com/bidnamu/bidnamubackend/auth/config/SecurityConfig.java
@@ -5,29 +5,38 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.filter.CorsFilter;
 
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
-    private final CorsFilter corsFilter;
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  private final CorsFilterConfig corsFilterConfig;
+  private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+  private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
-        return httpSecurity
-                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(registry -> registry.requestMatchers("/", "/**").permitAll())
-                .getOrBuild();
-    }
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+    return httpSecurity
+        .addFilterBefore(corsFilterConfig.corsFilter(), UsernamePasswordAuthenticationFilter.class)
+        .csrf(AbstractHttpConfigurer::disable)
+        .exceptionHandling(e -> {
+              e.accessDeniedHandler(jwtAccessDeniedHandler);
+              e.authenticationEntryPoint(jwtAuthenticationEntryPoint);
+            }
+        )
+        .sessionManagement(e -> e.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(registry -> registry.requestMatchers("/", "/**").permitAll())
+        .getOrBuild();
+
+  }
 }
