cd48612cd85dadc2d11558e739e0679ca3cc8dcf
kimmin1kk <rudals1888@naver.com>
2023-11-30 17:33:49 +0900

    refactor: User 엔티티가 리프레쉬 토큰을 갖는 구조로 변경, Authority @Setter 제거, 코드 수정

--- src/main/java/com/bidnamu/bidnamubackend/user/domain/User.java
+++ src/main/java/com/bidnamu/bidnamubackend/user/domain/User.java
@@ -8,6 +8,7 @@ import jakarta.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,41 +18,43 @@ import lombok.ToString;
 @Getter
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class User extends BaseTimeEntity {
 
-    @Column(nullable = false, unique = true)
-    private String email;
+  @Column(nullable = false, unique = true)
+  private String email;
 
-    @Column(nullable = false)
-    private String password;
+  @Column(nullable = false)
+  private String password;
 
-    @Column(nullable = false, unique = true)
-    private String nickname;
+  @Column(nullable = false, unique = true)
+  private String nickname;
 
-    @Column(nullable = false)
-    private boolean enabled = true;
+  @Column(nullable = false)
+  private boolean enabled = true;
 
-    @Column(nullable = false)
-    private boolean expired = false;
+  @Column(nullable = false)
+  private boolean expired = false;
 
-    @ToString.Exclude
-    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
-    private final Set<Authority> authorities = new HashSet<>();
+  @Column
+  private String refreshToken;
 
-    @Builder
-    public User(String email, String password, String nickname) {
-        this.email = email;
-        this.password = password;
-        this.nickname = nickname;
+  @ToString.Exclude
+  @OneToMany(mappedBy = "user", fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST,
+      CascadeType.REMOVE})
+  private final Set<Authority> authorities = new HashSet<>();
+
+  public void addAuthority(Role role) {
+    if (!this.authorities.isEmpty() && (authorities.stream()
+        .anyMatch(authority -> authority.getRole().equals(role)))) {
+      throw new IllegalArgumentException("해당 유저는 이미 해당 권한을 가지고 있습니다.");
     }
+    Authority authority = Authority.builder().role(role).user(this).build();
+    this.authorities.add(authority);
+  }
 
-    public void addAuthority(Role role) {
-        if (!this.authorities.isEmpty() && (authorities.stream().anyMatch(authority -> authority.getRole().equals(role)))) {
-                throw new IllegalArgumentException("해당 유저는 이미 해당 권한을 가지고 있습니다.");
-        }
-            Authority authority = new Authority();
-            authority.setRole(role);
-            authority.setUser(this);
-            this.authorities.add(authority);
-    }
+  public void updateRefreshToken(String refreshToken) {
+    this.refreshToken = refreshToken;
+  }
 }
