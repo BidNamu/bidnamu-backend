3a31512943d18973301ce89895acaa3d6930ab75
kimmin1kk <rudals1888@naver.com>
2023-12-04 20:10:40 +0900

    fix: 토큰 필터가 SecurityConfig에 적용되지 않은 문제 해결

--- src/main/java/com/bidnamu/bidnamubackend/auth/config/SecurityConfig.java
+++ src/main/java/com/bidnamu/bidnamubackend/auth/config/SecurityConfig.java
@@ -19,6 +19,7 @@ public class SecurityConfig {
   private final CorsFilter corsFilter;
   private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
   private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+  private final TokenAuthenticationFilter tokenAuthenticationFilter;
 
   @Bean
   public PasswordEncoder passwordEncoder() {
@@ -29,6 +30,7 @@ public class SecurityConfig {
   public SecurityFilterChain filterChain(final HttpSecurity httpSecurity) throws Exception {
     return httpSecurity
         .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+        .addFilterBefore(tokenAuthenticationFilter, CorsFilter.class)
         .csrf(AbstractHttpConfigurer::disable)
         .exceptionHandling(e -> {
               e.accessDeniedHandler(jwtAccessDeniedHandler);
