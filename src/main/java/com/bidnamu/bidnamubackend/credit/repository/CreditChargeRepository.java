f45642626924846f0c3b4c386abf85e8c31d5efb
Kang <Kang815595@gmail.com>
2024-01-09 19:44:45 +0900

    feat: 크레딧 충전기능 구현

---
+++ src/main/java/com/bidnamu/bidnamubackend/credit/repository/CreditChargeRepository.java
@@ -0,0 +1,8 @@
+package com.bidnamu.bidnamubackend.credit.repository;
+
+import com.bidnamu.bidnamubackend.credit.domain.CreditCharge;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CreditChargeRepository extends CrudRepository<CreditCharge, Long> {
+
+}
