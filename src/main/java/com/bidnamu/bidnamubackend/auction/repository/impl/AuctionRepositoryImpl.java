64b105a003f9d9a6bca258d53aa021903ccbf232
leehyeonwoo <dlgusdn7706@gmail.com>
2024-01-22 22:19:36 +0900

    refactor: 패키지 명 변경 및 코드 정렬

--- src/main/java/com/bidnamu/bidnamubackend/auction/repository/impl/AuctionRepositoryImpl.java
+++ src/main/java/com/bidnamu/bidnamubackend/auction/repository/impl/AuctionRepositoryImpl.java
@@ -1,4 +1,4 @@
-package com.bidnamu.bidnamubackend.auction.repository.repositoryImpl;
+package com.bidnamu.bidnamubackend.auction.repository.impl;
 
 import static com.bidnamu.bidnamubackend.auction.domain.QAuction.auction;
 
@@ -13,46 +13,35 @@ import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.util.StringUtils;
 
-public class AuctionRepositoryImpl extends QuerydslRepositorySupport implements
-    AuctionRepositoryCustom {
+public class AuctionRepositoryImpl implements AuctionRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
 
     public AuctionRepositoryImpl(final JPAQueryFactory queryFactory) {
-        super(Auction.class);
         this.queryFactory = queryFactory;
     }
 
     @Override
     public Page<Auction> findBySearchAuction(final SearchAuctionRequestDto requestDto,
         final Pageable pageable) {
-        JPAQuery<Auction> query = queryFactory
-            .selectFrom(auction)
-            .where(eqTitle(requestDto.title()),
-                eqCategoryName(requestDto.name()),
+        JPAQuery<Auction> query = queryFactory.selectFrom(auction)
+            .where(eqTitle(requestDto.title()), eqCategoryName(requestDto.name()),
                 eqAuctionStatus(requestDto.auctionStatus()),
                 eqRangePrice(requestDto.startPrice(), requestDto.closePrice()),
                 eqRangeTime(requestDto.startTime(), requestDto.closeTime()));
 
-
         if (requestDto.sortMethod() != null) {
-            query = eqSortMethod(query, requestDto.sortMethod());
+            query = eqSortMethod(query, requestDto.sortMethod().name());
         }
 
-        final List<Auction> content = query
-            .offset(pageable.getOffset())
-            .limit(pageable.getPageSize())
-            .fetch();
+        final List<Auction> content = query.offset(pageable.getOffset())
+            .limit(pageable.getPageSize()).fetch();
 
-        final JPAQuery<Long> countQuery = queryFactory
-            .select(auction.count())
-            .from(auction)
-            .where(eqTitle(requestDto.title()),
-                eqCategoryName(requestDto.name()),
+        final JPAQuery<Long> countQuery = queryFactory.select(auction.count()).from(auction)
+            .where(eqTitle(requestDto.title()), eqCategoryName(requestDto.name()),
                 eqAuctionStatus(requestDto.auctionStatus()),
                 eqRangePrice(requestDto.startPrice(), requestDto.closePrice()),
                 eqRangeTime(requestDto.startTime(), requestDto.closeTime()));
@@ -68,10 +57,10 @@ public class AuctionRepositoryImpl extends QuerydslRepositorySupport implements
         return StringUtils.hasText(title) ? auction.title.contains(title) : null;
     }
 
-    private BooleanExpression eqAuctionStatus(final String auctionStatus) {
-        return StringUtils.hasText(auctionStatus) ? auction.status.eq(
-            AuctionStatus.valueOf(auctionStatus)) : null;
+    private BooleanExpression eqAuctionStatus(final Enum<AuctionStatus> auctionStatus) {
+        return auctionStatus != null ? auction.status.eq((AuctionStatus) auctionStatus) : null;
     }
+
     private BooleanExpression eqRangePrice(final int startPrice, final int closePrice) {
         return startPrice != 0 && closePrice != 0 ? auction.currentBid.between(startPrice,
             closePrice) : null;
