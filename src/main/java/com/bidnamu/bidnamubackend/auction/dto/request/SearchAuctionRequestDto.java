f4315e3121c7e7b7b97078d1043d934e52031ffe
leehyeonwoo <dlgusdn7706@gmail.com>
2024-01-22 22:22:29 +0900

    refactor: 검증하는 절차 추가 및 코드 정렬

--- src/main/java/com/bidnamu/bidnamubackend/auction/dto/request/SearchAuctionRequestDto.java
+++ src/main/java/com/bidnamu/bidnamubackend/auction/dto/request/SearchAuctionRequestDto.java
@@ -1,25 +1,35 @@
 package com.bidnamu.bidnamubackend.auction.dto.request;
 
+import com.bidnamu.bidnamubackend.auction.domain.AuctionSortMethod;
+import com.bidnamu.bidnamubackend.auction.domain.AuctionStatus;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.PositiveOrZero;
 import java.time.LocalDateTime;
 
 public record SearchAuctionRequestDto(
     String name,
     String title,
+    @PositiveOrZero
     int startPrice,
+    @PositiveOrZero
     int closePrice,
-    String auctionStatus,
-    String sortMethod,
+    AuctionStatus auctionStatus,
+    AuctionSortMethod sortMethod,
+    @PastOrPresent
     LocalDateTime startTime,
+    @PastOrPresent
     LocalDateTime closeTime
 ) {
+
     public static SearchAuctionRequestDto of(
         final String name,
         final String title,
-        final String auctionStatus,
-        final String sortMethod,
+        final AuctionStatus auctionStatus,
+        final AuctionSortMethod sortMethod,
         final LocalDateTime startTime,
         final LocalDateTime closeTime
     ) {
-        return new SearchAuctionRequestDto(name, title, 0, 0,auctionStatus,sortMethod,startTime, closeTime);
+        return new SearchAuctionRequestDto(name, title, 0, 0, auctionStatus, sortMethod, startTime,
+            closeTime);
     }
 }
