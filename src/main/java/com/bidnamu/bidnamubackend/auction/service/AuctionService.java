42a1f199a4e73ad22b60ba3defdfaaff1786987b
Jeongmu <Kang815595@gmail.com>
2024-01-30 20:05:05 +0900

    refactor: 사용되지 않는 로깅 어노테이션 제거

--- src/main/java/com/bidnamu/bidnamubackend/auction/service/AuctionService.java
+++ src/main/java/com/bidnamu/bidnamubackend/auction/service/AuctionService.java
@@ -21,13 +21,11 @@ import com.bidnamu.bidnamubackend.user.service.UserService;
 import java.util.List;
 import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class AuctionService {
 
     private final AuctionRepository auctionRepository;
