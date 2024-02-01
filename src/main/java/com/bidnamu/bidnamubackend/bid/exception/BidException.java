c79d51ac6e68ac8e32f1ad6868e13306898091af
Jeongmu <Kang815595@gmail.com>
2024-02-01 18:17:07 +0900

    refactor: 사용되지 않는 생성자 제거

--- src/main/java/com/bidnamu/bidnamubackend/bid/exception/BidException.java
+++ src/main/java/com/bidnamu/bidnamubackend/bid/exception/BidException.java
@@ -2,9 +2,6 @@ package com.bidnamu.bidnamubackend.bid.exception;
 
 public class BidException extends RuntimeException {
 
-    public BidException() {
-    }
-
     public BidException(final String message) {
         super(message);
     }
